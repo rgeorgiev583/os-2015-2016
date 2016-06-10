ddad4a28ee7c3dbe48de37f23b19c4549467c2d4
Radoslav Georgiev <rgeorgiev583@gmail.com>
2016-06-11 02:17:32 +0300

    optimize: dropped a redundant var assignment

--- 1.1.sh
+++ 1.1.sh
@@ -2,10 +2,9 @@
 
 if [ $1 = -s ]
 then
-    sizes=$(find ${@:3} -name "*.$2" -type f -exec wc -c {} \; | cut -f1 -d' ')
     total=0
 
-    for size in $sizes
+    for size in $(find ${@:3} -name "*.$2" -type f -exec wc -c {} \; | cut -f1 -d' ')
     do
         total=$(($total + $size))
     done
