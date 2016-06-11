fedab84dbc72455bfe7c1a4f2345a5340a339495
Radoslav Georgiev <rgeorgiev583@gmail.com>
2016-06-11 11:33:16 +0300

    replaced echo with kill in complex shell script

--- 1.2.sh
+++ 1.2.sh
@@ -10,11 +10,11 @@ if [ $1 = -n ]
 then
     for user in ${@:3}
     do
-        echo $(ps aux | grep ^$user | grep $2 | awk '{ print $2 }')
+        kill $(ps aux | grep ^$user | grep $2 | awk '{ print $2 }')
     done
 else
     for user in $@
     do
-        echo $(ps aux | grep ^$user | awk '{ print $2 }')
+        kill $(ps aux | grep ^$user | awk '{ print $2 }')
     done
 fi
