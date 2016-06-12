741ba318c7b4515e134817a22e6bda93753c7e0f
Radoslav Georgiev <rgeorgiev583@gmail.com>
2016-06-12 19:36:51 +0300

    81187: commented and graded the second shell script task

--- midterm/81187/ukill.sh
+++ midterm/81187/ukill.sh
@@ -1,4 +1,17 @@
+#!/bin/bash
+
+# A simple and elegant solution! Well done!  (+0.1)
+
 for user
 do
+    #   It does not make sense to pipe `awk`'s output to `kill`.
+    # It should be provided as arguments to `kill` instead using `xargs`.
+    # (-0.1)
+    # Like this:
+	#ps aux | grep ^$user | awk '{print $2}' | xargs kill
+    # Or:
+    #kill $(ps aux | grep ^$user | awk '{print $2}')
 	ps aux | grep $user | awk '{print $2}' | kill
-done 
+done
+
+# Total points for this task:  1/1
