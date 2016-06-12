439bc8d435b5b3752d95e20d82b2aae3117fdb2c
Radoslav Georgiev <rgeorgiev583@gmail.com>
2016-06-12 21:40:59 +0300

    81104: commented and graded the first shell script task

--- midterm/81104/ttys.sh
+++ midterm/81104/ttys.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 #usage ttys USER...
 
+# Validation! (+0.1)
+
 if [[ $# -eq 0 ]]
 then
 	exit 1 #Error: too few arguments
 fi
-else
+#else  # Fixed a syntax error.
 
 for user
 do
+    #   `cut` won't function properly here; `awk` is more suitable when
+    # you've got delimiters wider than one character.  (-0.1)
+    # Like this:
+	#who | grep "$user" | awk '{ print $2 }'
 	who | grep "$user" | cut -f2 -d$'\t'
 done
+
+# Total points for this task:  1/1
