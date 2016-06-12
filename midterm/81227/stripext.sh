aaa04851219d6659fa6c10ee707f4e4b9ee87268
Radoslav Georgiev <rgeorgiev583@gmail.com>
2016-06-12 15:45:33 +0300

    81227: fixed some details about the fourth shell script task

--- midterm/81227/stripext.sh
+++ midterm/81227/stripext.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
-#   Files provided as arguments are not handled at all!  (-0.2)
+#   Files provided as arguments are not handled at all!  (-0.1)
 if [ $# -eq 0 ]; then
 #   It is preferable to use `read -r` to just `read`.
 while read line
 do
-#   `cut` is not enough in this case; you have to use variable substitutions,
+#   `cut` is not useful in this case; you have to use variable substitutions,
 # like this:  (-0.3)
 #line=${line##*/}
 #line=${line%%.*}
+#   Also, `$line` should be piped into `cut`'s input, not passed as an argument.
+# Like this:  (-0.2)
+#echo "$line" | cut -f1 -d "."
 cut -f1 -d "." "$line"
 done
 fi
 
-# Total points for this task:  0.5/1
+# Total points for this task:  0.4/1
