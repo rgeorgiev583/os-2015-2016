22ccd827300cb2cf7b064ed14409b4719cd0c704
Radoslav Georgiev <rgeorgiev583@gmail.com>
2016-06-12 21:42:06 +0300

    81104: commented and graded the fourth shell script task

--- midterm/81104/finddir.sh
+++ midterm/81104/finddir.sh
@@ -1,24 +1,43 @@
-#!/bin/bash/
+#!/bin/bash
 #usage finddir FILE...
 
 function getDir(){
 	local filename=$1
+    #   Interesting solution!  (+0.1)
+    # But you could have used the following one, which is simpler:
+    #filename=${filename%/*}
+    #echo ${filename##*/}
 	echo "$filename" | rev | cut -d'/' -f2 | rev
 }
 
+# There's boilerplate code in the `if` and `else` clauses...
 if [[ $# -eq 0 ]]
 then
+    # That's redundant! You can fully omit the `input` variable.
 	input=$(cat)
-	while read -r line
-	do
+    # One of the `while`s is useless.  (-0.1)
+    # `-r` is used!  Congrats!  (+0.1)
+	#while read -r line
+	#do
 		while read -r p
 		do
 			getDir $p
-		done <$line
+		#done <$line
 	done < $input
-fi
+    #   The upper line could just have been `done`, without `$input`.
+    #   Also, `$input` is not passed correctly to the `while`.  The statement
+    # should have been the following:  (-0.1)
+    #while read -r p
+    #do
+        #getDir $p
+    #done
+    # Or, alternatively, `$input` may be piped into `while` like this:
+    #echo $input | while read -r p
+    #...
+#fi  # If you must have an `else` clause, do not put a `fi` before it.  (-0.1)
 
 else
+    # See the notes inside the `if` clause.
 	for filename
 	do
 		while read -r p
@@ -27,3 +46,5 @@ else
 		done <$filename
 	done
 fi
+
+# Total points for this task:  0.9/1
