ab8fea2076bd52e47bd5638d9aa4681b11405153
Radoslav Georgiev <rgeorgiev583@gmail.com>
2016-06-12 19:46:36 +0300

    midterm: fixed grade calculation script

--- midterm/grade.sh
+++ midterm/grade.sh
@@ -24,4 +24,4 @@ bash_grade=$(gradesection sh $path)
 c_grade=$(gradesection c $path)
 echo Total grade for Bash section: $bash_grade
 echo Total grade for C section: $c_grade
-echo Final grade: $(echo \($bash_grade + $c_grade\) / 2 | bc)
+echo Final grade: $(echo "scale=2; ($bash_grade + $c_grade) / 2" | bc)
