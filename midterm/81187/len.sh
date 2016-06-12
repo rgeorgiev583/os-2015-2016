c37be4dca6c509b4a0a05432bcea360affba71bc
Radoslav Georgiev <rgeorgiev583@gmail.com>
2016-06-12 19:35:49 +0300

    81187: commented and graded the first shell script task

--- midterm/81187/len.sh
+++ midterm/81187/len.sh
@@ -1,4 +1,10 @@
+#!/bin/bash
+
+# Your script does not support multiple directories as arguments!  (-0.1)
+
 for i in $(find $2 -type f | grep $1$)
 do
 	echo $(wc -c < $i)
 done
+
+# Total points for this task:  0.9/1
