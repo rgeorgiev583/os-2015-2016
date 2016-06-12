4b9d4310aedbd5cad0e7a29e037bed611ff5db08
Radoslav Georgiev <rgeorgiev583@gmail.com>
2016-06-12 15:33:17 +0300

    81227: commented and graded the fourth shell script task

--- midterm/81227/stripext.sh
+++ midterm/81227/stripext.sh
@@ -1,6 +1,16 @@
+#!/bin/bash
+
+#   Files provided as arguments are not handled at all!  (-0.2)
 if [ $# -eq 0 ]; then
+#   It is preferable to use `read -r` to just `read`.
 while read line
 do
+#   `cut` is not enough in this case; you have to use variable substitutions,
+# like this:  (-0.3)
+#line=${line##*/}
+#line=${line%%.*}
 cut -f1 -d "." "$line"
 done
 fi
+
+# Total points for this task:  0.5/1
