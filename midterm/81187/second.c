1fe5d29cc4900787233dfdafed752dfa8212d062
Radoslav Georgiev <rgeorgiev583@gmail.com>
2016-06-12 19:39:35 +0300

    81187: commented and graded the second C task

--- midterm/81187/second.c
+++ midterm/81187/second.c
@@ -1,16 +1,31 @@
+// `cgrep` implementation
+
 #include <fcntl.h>
 #include <stdlib.h>
 #include <stdio.h>
-#include <string>
+#include <string.h>
+// `.h` was omitted.
+#include <unistd.h>
+// This header is necessary, too.
+
+// There is no concurrency (no `fork`, no `wait`).  (-0.5)
+// At least part of the extra credit is implemented.  (+0.5)
 
 int main (int argc, char* argv[])
 {
+    //   A bit too much boilerplate code... it is quite a nuisance to read.
+    // If you hadn't copied code, it would be much easier for you to correct
+    // your mistakes.
     if(strcmp(argv[1], "-c")==0)
     {
         int fds[1000];
         for (int i = 3; i < argc; ++i)
         {
             char *filename = argv[i];
+            //   This is absolutely a wrong way to open the files!
+            // You open them for reading, so the most relevant flag to use is
+            // `O_RDONLY`!  (-0.2)
+            //fds[i - 3] = open(filename, O_RDONLY);
             fds[i - 3] = open(filename, O_WRONLY, 0644);
         }
 
@@ -19,18 +34,25 @@ int main (int argc, char* argv[])
         int times=0;
         for (int i = 3; i < argc; ++i)
         {
+            // The fd number is wrong (should have been fds[i - 3])!
             while( read(fds[i], &c, 1) )
             {
+                //   This should have been the second argument.
+                // (i.e. (argv[2]))  (-0.1)
                 if(c==(argv[1])[0])
                     times++;
             }
         }
-        printf("%t/n":times);
+        //   The format specifiers are wrong!
+        // Also, `/n` should have been `\n`.  (-0.2)
+        //printf("%d\n", times);
+        printf("%t/n", times);  // Fixed a typo.
 
         for (int i = 3; i < argc; ++i)
             close(fds[i - 3]);
     }
 
+    // See the notes inside the `if` clause.
     else
     {
 
@@ -38,6 +60,10 @@ int main (int argc, char* argv[])
         for (int i = 2; i < argc; ++i)
         {
             char *filename = argv[i];
+            //   This is absolutely a wrong way to open the files!
+            // You open them for reading, so the most relevant flag to use is
+            // `O_RDONLY`!
+            //fds[i - 2] = open(filename, O_RDONLY);
             fds[i - 2] = open(filename, O_WRONLY, 0644);
         }
 
@@ -46,12 +72,16 @@ int main (int argc, char* argv[])
         for (int i = 2; i < argc; ++i)
         {
             int times=0;
+            // The fd number is wrong (should have been fds[i - 2])!
             while( read(fds[i], &c, 1) )
             {
                 if(c==(argv[1])[0])
                     times++;
             }
-            printf("%t/n":times);
+            //   The format specifiers are wrong!
+            // Also, `/n` should have been `\n`.
+            //printf("%d\n", times);
+            printf("%t/n", times);  // Fixed a typo.
         }
 
         for (int i = 2; i < argc; ++i)
@@ -60,3 +90,5 @@ int main (int argc, char* argv[])
 
     return 0;
 }
+
+// Total points for this task:  1.5/2
